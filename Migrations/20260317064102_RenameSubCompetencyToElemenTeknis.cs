6f1a090299d0f0dbe3f9dee02e39fd0f54b1d536
Rino <rinoadi28@gmail.com>
2026-03-17 14:41:45 +0800

    chore(183-01): add EF migration RenameSubCompetencyToElemenTeknis
    - RenameColumn PackageQuestions.SubCompetency -> ElemenTeknis
    - Migration applied to DB via dotnet ef database update
    - Build: 0 errors, 0 warnings

---
+++ Migrations/20260317064102_RenameSubCompetencyToElemenTeknis.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HcPortal.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameSubCompetencyToElemenTeknis : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SubCompetency",
+                table: "PackageQuestions",
+                newName: "ElemenTeknis");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ElemenTeknis",
+                table: "PackageQuestions",
+                newName: "SubCompetency");
+        }
+    }
+}
