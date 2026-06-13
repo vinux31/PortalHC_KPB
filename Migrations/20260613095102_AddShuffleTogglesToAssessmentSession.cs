75f81512babcc0ffd3e3b9280203d243095fc714
Rino <rinoadi28@gmail.com>
2026-06-13 17:54:01 +0800

    feat(372-01): add+apply migration AddShuffleTogglesToAssessmentSession (bit DEFAULT 1, backfill 58 rows ON)

---
+++ Migrations/20260613095102_AddShuffleTogglesToAssessmentSession.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HcPortal.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShuffleTogglesToAssessmentSession : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ShuffleOptions",
+                table: "AssessmentSessions",
+                type: "bit",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ShuffleQuestions",
+                table: "AssessmentSessions",
+                type: "bit",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShuffleOptions",
+                table: "AssessmentSessions");
+
+            migrationBuilder.DropColumn(
+                name: "ShuffleQuestions",
+                table: "AssessmentSessions");
+        }
+    }
+}
